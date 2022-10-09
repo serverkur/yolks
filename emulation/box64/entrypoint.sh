f13b0273f44a5bd60e96e7c97b95a758081fb340
Quinten <josdekurk@gmail.com>
2022-10-09 13:01:35 +0200

    Cleanup repo
    Cleanup my repo to only images I need

--- emulation/box64/entrypoint.sh
+++ emulation/box64/entrypoint.sh
@@ -4,9 +4,9 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $(NF-2);exit}'`
 
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mdotnet --info\n"
-dotnet --info
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mAMD64 Emualtor image box64\n"
 
+box64 --version
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
