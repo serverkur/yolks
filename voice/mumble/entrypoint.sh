cc2b4d3bba67afbbc9401eebf7e63287bab5ef1b
Quinten <67589015+QuintenQVD0@users.noreply.github.com>
2022-10-13 18:59:27 +0200

    fix entrypoint mumble

--- voice/mumble/entrypoint.sh
+++ voice/mumble/entrypoint.sh
@@ -5,7 +5,7 @@ cd /home/container
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-mumble --version
+mumble-server --version
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
