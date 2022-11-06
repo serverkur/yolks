a0aa3e9d49ab7a46a2c078855542ebc6aee76aa6
QuintenQVD0 <josdekurk@gmail.com>
2022-11-06 17:25:14 +0100

    do it this way

--- dotnet/entrypoint.sh
+++ dotnet/entrypoint.sh
@@ -7,6 +7,7 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $(NF-2);exit}'`
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mdotnet --info\n"
 dotnet --info
 
+export DOTNET_ROOT=/usr/share/
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
