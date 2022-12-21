6e43c33e5da2d1206fbd96e470a1171605a25dea
Quinten <67589015+QuintenQVD0@users.noreply.github.com>
2022-12-21 11:09:17 +0100

    Update entrypoint.sh

--- temp/redis_7/entrypoint.sh
+++ temp/redis_7/entrypoint.sh
@@ -10,7 +10,7 @@ MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 echo "starting the redis server"
-/usr/local/bin/redis-server /home/container/redis.conf --save 60 1 --dir /home/container/ --bind 0.0.0.0 --port ${SERVER_PORT} --requirepass ${SERVER_PASSWORD} --maxmemory ${SERVER_MEMORY}mb --daemonize yes
+/usr/local/bin/redis-server /home/container/redis.conf --save 60 1 --dir /home/container/ --bind 172.18.0.1 --port ${SERVER_PORT} --requirepass "{{SERVER_PASSWORD}}" --maxmemory ${SERVER_MEMORY}mb --daemonize yes
 ss -lntp | grep ${SERVER_PORT}
 sleep 2
 echo "connecting with the cli"
